dfbe0cf8907d2d4a6c37d79c2b35e6b0ca495198
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-09-27 20:09:09 -0400

    Adjust icon & name terms.

--- js/context.js
+++ js/context.js
@@ -6,18 +6,15 @@ module.exports = {
     type: '@type',
     '@protected': true,
 
+    displayIcon: {
+      '@id': 'https://schema.org/image',
+      '@type': '@id'
+    },
+
+    displayName: 'https://schema.org/name',
+
     LoginDisplayCredential: {
-      '@id': 'https://w3id.org/xr#LoginDisplayCredential',
-      '@context': {
-        '@protected': true,
-
-        displayIcon: {
-          '@id': 'https://schema.org/image',
-          '@type': '@id'
-        },
-
-        displayName: 'https://schema.org/name'
-      }
+      '@id': 'https://w3id.org/xr#LoginDisplayCredential'
     },
     UserPreferencesCredential: {
       '@id': 'https://w3id.org/xr#UserPreferencesCredential',
